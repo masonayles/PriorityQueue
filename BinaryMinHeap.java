7c51592fab7cc5e70c3d8b9487fb4c8d88284862
mayles21 <mayles21@georgefox.edu>
2023-11-26 20:42:16 -0800

    Initial commit. 11/26/23

--- BinaryMinHeap.java
+++ BinaryMinHeap.java
@@ -1,4 +1,156 @@
-public class BinaryMinHeap<E>
+/**
+ *
+ * @param <E>
+ */
+public class BinaryMinHeap<E extends Comparable<E>> implements Iterable<E>
 {
-    // TODO implement class
+    private E[] _heap;
+    private int _size;
+    private static final int DEFAULT = 10;
+
+    /**
+     *
+     * @param initialCapacity
+     */
+    public BinaryMinHeap(int initialCapacity)
+    {
+        if (initialCapacity < 1)
+        {
+            throw new IllegalArgumentException();
+        }
+        _heap
+    }
+
+    /**
+     * 
+     */
+    public BinaryMinHeap()
+    {
+        this(DEFAULT);
+    }
+
+    /**
+     *
+     * @param element
+     */
+    public void add(E element)
+    {
+
+    }
+
+    /**
+     *
+     * @return
+     */
+    public E get()
+    {
+
+    }
+
+    /**
+     *
+     * @return
+     */
+    public E remove()
+    {
+
+    }
+
+    /**
+     *
+     */
+    public void clear()
+    {
+        _size = 0;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int size()
+    {
+        return _size;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public boolean isEmpty()
+    {
+        return _size == 0;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Iterator<E> iterator()
+    {
+        return new BinaryMinHeapIterator();
+    }
+
+    /**
+     *
+     * @param index
+     */
+    private void siftUp(int index)
+    {
+
+    }
+
+    /**
+     *
+     * @param index
+     */
+    private void siftDown(int index)
+    {
+
+    }
+
+    /**
+     *
+     */
+    private void grow()
+    {
+
+    }
+
+    /**
+     *
+     */
+    private class BinaryMinHeapIterator implements Iterator<E>
+    {
+        private int _index = 0;
+
+        /**
+         *
+         * @return
+         */
+        public boolean hasNext()
+        {
+
+        }
+
+        /**
+         *
+         * @return
+         */
+        public E next()
+        {
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
